1bc6796de1b29e5b1de9d317728c8b82bff04ae9
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-06-15 17:49:26 +0900

    change field

--- src/main/java/com/domain/User.java
+++ src/main/java/com/domain/User.java
@@ -3,19 +3,28 @@ package com.domain;
 public class User {
 
 	private String name;
-	private Integer age;
+	private String hobby;
+	private String birthday;
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getAge() {
-		return age;
+	public String getHobby() {
+		return hobby;
 	}
-	public void setAge(Integer age) {
-		this.age = age;
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
 	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
+	
 	
 	
 }
